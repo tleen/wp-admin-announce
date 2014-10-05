9c5a314138332d24c4af5115d083fe5cd41642f3
Thomas Leen <tl@thomasleen.com>
2014-10-05 13:25:26 -0400

    added div on top of body

--- wp-admin-announce.js
+++ wp-admin-announce.js
@@ -1,5 +1,9 @@
 this.adminAnnounce = function(message){
-  jQuery(document).ready(function(){
+  jQuery(document).ready(function(){    
     console.log('admin announcing:', message);
+    jQuery('body').prepend(
+      jQuery('<div>')
+	.text(message)
+	.addClass('admin-announce'));
   });
 };
